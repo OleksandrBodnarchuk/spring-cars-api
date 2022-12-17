abe9416d08c120ec4020751817e4492d97e0736a
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2022-12-17 15:13:31 +0100

    Set up H2 for tests

--- src/main/java/pl/alex/cars/entity/Manufacturer.java
+++ src/main/java/pl/alex/cars/entity/Manufacturer.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Entity
 public class Manufacturer {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Long manufacturer_value;
