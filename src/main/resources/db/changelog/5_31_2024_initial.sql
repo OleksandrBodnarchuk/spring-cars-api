58f62877db8a7437bf62b99adefc121b555801f0
Alex <oleksandr.bodnarchuk@co3.eu>
2024-05-31 13:27:05 +0200

    Resolves #9 - initial changeset updated

--- src/main/resources/db/changelog/5_31_2024_initial.sql
+++ src/main/resources/db/changelog/5_31_2024_initial.sql
@@ -1,3 +1,5 @@
+--liquibase formatted sql
+--changeset alex:5_31_2024_initial
 create sequence body_sequence start with 1 increment by 10;
 create sequence brand_sequence start with 1 increment by 10;
 create sequence chasis_sequence start with 1 increment by 10;
@@ -102,35 +104,35 @@ create table running_feature
 
 alter table if exists model
     add constraint FKhbgv4j3vpt308sepyq9q79mhu
-    foreign key (brand_id)
-    references brand;
+        foreign key (brand_id)
+            references brand;
 
 alter table if exists modification
     add constraint FKkqmi0f5a34vx9brvmdqikg9d4
-    foreign key (body_id)
-    references body;
+        foreign key (body_id)
+            references body;
 
 alter table if exists modification
     add constraint FKbot7u4kq7kkhnxypipgj6j3af
-    foreign key (chassis_id)
-    references chasis;
+        foreign key (chassis_id)
+            references chasis;
 
 alter table if exists modification
     add constraint FKqocbq3mbqjq2xpybdiibbpkt6
-    foreign key (engine_id)
-    references engine;
+        foreign key (engine_id)
+            references engine;
 
 alter table if exists modification
     add constraint FKfuk89v1d6dceo0er3120tgusd
-    foreign key (general_info_id)
-    references general_info;
+        foreign key (general_info_id)
+            references general_info;
 
 alter table if exists modification
     add constraint FK9uff0co2pqf3nqynpf9eq2d8l
-    foreign key (model_id)
-    references model;
+        foreign key (model_id)
+            references model;
 
 alter table if exists modification
     add constraint FKix4mfjxc1ue5hr4ht5kmu3vjx
-    foreign key (running_feature_id)
-    references running_feature;
+        foreign key (running_feature_id)
+            references running_feature;
