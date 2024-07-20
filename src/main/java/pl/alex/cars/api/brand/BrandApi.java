c7681cd3d68137987df10e0b423996806f018e14
Oleksandr Bodnarchuk <58620267+OleksandrBodnarchuk@users.noreply.github.com>
2024-07-20 10:56:34 +0200

    11 fill in brand data (#14)
    * 9 add liquibase (#10)
    
    * #9 - added Liquibase to project with init .sql [x]
    
    * Resolves #9 - initial changeset updated
    
    ---------
    
    Co-authored-by: Alex <oleksandr.bodnarchuk@co3.eu>
    
    * Closes #11 - db inserts + changes in API
    
    ---------
    
    Co-authored-by: Alex <oleksandr.bodnarchuk@co3.eu>

--- src/main/java/pl/alex/cars/api/brand/BrandApi.java
+++ src/main/java/pl/alex/cars/api/brand/BrandApi.java
@@ -1,14 +1,13 @@
 package pl.alex.cars.api.brand;
 
 import java.io.IOException;
-import org.springframework.data.domain.Page;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import pl.alex.cars.api.brand.dto.BrandRequest;
 import pl.alex.cars.api.brand.dto.BrandResponse;
 
@@ -28,13 +27,12 @@ public interface BrandApi {
 
   @RequestMapping(value = "/brands/{name}",
       produces = {"application/json"},
-      method = RequestMethod.POST)
+      method = RequestMethod.GET)
   ResponseEntity<BrandResponse> getBrandByName(@PathVariable("name") String brandName);
 
   @RequestMapping(value = "/brands",
       produces = {"application/json"},
-      method = RequestMethod.POST)
-  ResponseEntity<Page<BrandResponse>> getAllBrands(@RequestParam(value = "page", defaultValue = "0") Integer page,
-      @RequestParam(value = "size", defaultValue = "10") Integer size);
+      method = RequestMethod.GET)
+  ResponseEntity<List<String>> getBrands();
 
 }
--- src/main/java/pl/alex/cars/api/brand/repository/BrandRepository.java
+++ src/main/java/pl/alex/cars/api/brand/repository/BrandRepository.java
@@ -2,12 +2,11 @@ package pl.alex.cars.api.brand.repository;
 
 import java.util.List;
 import java.util.Optional;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import pl.alex.cars.api.brand.entity.Brand;
 
-public interface BrandRepository extends PagingAndSortingRepository<Brand, Long> {
-	
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+
 	Optional<Brand> findByNameEquals(String name);
 
 	List<Brand> findBrandByNameIn(List<String> names);
